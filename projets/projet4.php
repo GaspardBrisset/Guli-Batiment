8efd84865ea539423ec2fbd2ff9625e90b745981
GBrisset <brissetgaspardgmail.com>
2025-08-29 23:02:18 +0200

    push v3

--- projets/projet4.php
+++ projets/projet4.php
@@ -11,11 +11,15 @@
     <p>
     </p>
     <div class="project-gallery">
-        <img src="../images/image0000001.jpeg" alt="Aménagement extérieur 1">
-        <img src="../images/IMG_2506.jpeg" alt="Aménagement extérieur 2">
-        <img src="../images/IMG_2508.jpeg" alt="Aménagement extérieur 3">
-        <img src="../images/IMG_3561.jpeg" alt="Aménagement extérieur 4">
-
+        <img src="../images/IMG_1301.jpeg" alt="Aménagement extérieur 1">
+        <img src="../images/IMG_1302.jpeg" alt="Aménagement extérieur 2">
+        <img src="../images/4902139F-44B8-4B1F-AD69-02D4F494B1F4.jpeg" alt="Aménagement extérieur 3">
+        <img src="../images/BEEBE7E1-6996-469A-87C9-BD13313A42AE.jpeg" alt="Aménagement extérieur 4">
+        <img src="../images/IMG_0998.jpeg" alt="Aménagement extérieur 5">
+        <img src="../images/image0000001.jpeg" alt="Aménagement extérieur 6">
+        <img src="../images/IMG_2506.jpeg" alt="Aménagement extérieur 7">
+        <img src="../images/IMG_2508.jpeg" alt="Aménagement extérieur 8">
+        <img src="../images/IMG_3561.jpeg" alt="Aménagement extérieur 9">
     </div>
     <a href="../galerie.php" class="back-button">Retour à la galerie</a>
 </section>
